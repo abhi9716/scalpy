5ac6b190729e058b8b20fd74844501d1aaa96971
Abhishek Kumar <akumar@ch.iitr.ac.in>
2025-01-27 15:50:12 +0530

    Update delvery.py

--- pages/delvery.py
+++ pages/delvery.py
@@ -57,8 +57,8 @@ client = init_connection(usr,pwd)
 
 @st.cache_data(ttl=600)
 def get_data():
-    db = client.Delv_data
-    items = db.open_trades1.find()
+    db = client.Scalping_data
+    items = db.d8.find()
     #if len(list(items))==0:
         #items = db.d1.find()
     items = list(items)  # make hashable for st.cache_data
@@ -67,75 +67,96 @@ def get_data():
 
 df = get_data()
 st.write(df)
-# df["Datetime"] = pd.to_datetime(df["Date"],format='mixed',dayfirst=True)
-# df['Hour'] = df['Datetime'].apply(lambda x: x.hour)
-# df['Day'] = df['Datetime'].apply(lambda x: x.day_name())
-# df['Date'] = df['Datetime'].apply(lambda x: x.date())
-# df["pnl"] = ((df["sell_at"]-df["buy_at"])/df["buy_at"])*100
+df["Datetime"] = pd.to_datetime(df["Date"],format='mixed',dayfirst=True)
+df['Hour'] = df['Datetime'].apply(lambda x: x.hour)
+df['Day'] = df['Datetime'].apply(lambda x: x.day_name())
+df['Date'] = df['Datetime'].apply(lambda x: x.date())
+df["pnl"] = (df["per"])*100
 
 
-# df1 = df
-# df1 = df1.sort_values("Datetime")
-# df1["running_pnl"] = df1.groupby('Date').pnl.cumsum()
+df1 = df
+df1 = df1.sort_values("Datetime")
+df1["running_pnl"] = df1.groupby('Date').pnl.cumsum()
 
-# pivot = np.round(pd.pivot_table(df1, values='pnl', 
-#                                 index=['Date'], 
-#                                 columns=['Day'], 
-#                                 aggfunc=np.sum,
-#                                 fill_value=0),2)
+pivot = np.round(pd.pivot_table(df1, values='pnl', 
+                                index=['Date'], 
+                                columns=['Day'], 
+                                aggfunc=np.sum,
+                                fill_value=0),2)
 
-# pivot1 = np.round(pd.pivot_table(df1, values='pnl', 
-#                                 index=['Date'], 
-#                                 columns=['Hour'], 
-#                                 aggfunc=np.sum,
-#                                 fill_value=0),2)
+pivot1 = np.round(pd.pivot_table(df1, values='pnl', 
+                                index=['Date'], 
+                                columns=['Hour'], 
+                                aggfunc=np.sum,
+                                fill_value=0),2)
 
-# pivot2 = np.round(pd.pivot_table(df1, values='pnl', 
-#                                 index=['Day'], 
-#                                 columns=['Hour'], 
-#                                 aggfunc=np.sum,
-#                                 fill_value=0),2)
+pivot2 = np.round(pd.pivot_table(df1, values='pnl', 
+                                index=['Day'], 
+                                columns=['Hour'], 
+                                aggfunc=np.sum,
+                                fill_value=0),2)
 
-# pivot3 = np.round(pd.pivot_table(df1, values='pnl', 
-#                                 index=['Date'], 
-#                                 columns=['Hour'], 
-#                                 aggfunc='count',
-#                                 fill_value=0),2)
-# # Print results.
+pivot3 = np.round(pd.pivot_table(df1, values='pnl', 
+                                index=['Date'], 
+                                columns=['Hour'], 
+                                aggfunc='count',
+                                fill_value=0),2)
+# Print results.
 
-# @st.cache_data
-# def convert_df_to_csv(df):
-#   # IMPORTANT: Cache the conversion to prevent computation on every rerun
-#   return df.to_csv(index=False).encode('utf-8')
-
-
-# st.header('Sample Data')
-# st.write(df1)
-# st.download_button(
-#   label="Download data as CSV",
-#   data=convert_df_to_csv(df1),
-#   file_name='df.csv',
-#   mime='text/csv',
-# )
-
-# st.subheader('Date vs Day PNL')
-# st.write(pivot.style.applymap(highlight_max))
-# st.subheader('Date vs Hour PNL')
-# st.write(pivot1.style.applymap(highlight_max))
-# st.subheader('Day vs Hour PNL')
-# st.write(pivot2.style.applymap(highlight_max))
-# st.subheader('Date vs Hour Trades Count')
-# st.write(pivot3)
+@st.cache_data
+def convert_df_to_csv(df):
+  # IMPORTANT: Cache the conversion to prevent computation on every rerun
+  return df.to_csv(index=False).encode('utf-8')
 
 
+st.header('Sample Data')
+st.write(df1)
+st.download_button(
+  label="Download data as CSV",
+  data=convert_df_to_csv(df1),
+  file_name='df.csv',
+  mime='text/csv',
+)
+
+st.subheader('Date vs Day PNL')
+st.write(pivot.style.applymap(highlight_max))
+st.subheader('Date vs Hour PNL')
+st.write(pivot1.style.applymap(highlight_max))
+st.subheader('Day vs Hour PNL')
+st.write(pivot2.style.applymap(highlight_max))
+st.subheader('Date vs Hour Trades Count')
+st.write(pivot3)
 
 
 
-# # with st.sidebar:
-# #     with st.form(key='my_form', clear_on_submit=True):
-# #         uploaded_f = st.file_uploader("FILE UPLOADER")
-# #         submitted = st.form_submit_button("UPLOAD!")
+
+
+# with st.sidebar:
+#     with st.form(key='my_form', clear_on_submit=True):
+#         uploaded_f = st.file_uploader("FILE UPLOADER")
+#         submitted = st.form_submit_button("UPLOAD!")
         
-# #         if submitted and uploaded_f is not None:
-# #             st.write("UPLOADED!")
-# #             # do stuff with your uploaded file
+#         if submitted and uploaded_f is not None:
+#             st.write("UPLOADED!")
+#             # do stuff with your uploaded file
+
+
+
+# @st.cache
+# def load_data1(uploaded_f):
+#     data1 = pd.read_csv(uploaded_f)
+#     return data1
+
+
+# @st.experimental_memo(ttl=24 * 3600, persist="disk", show_spinner=False)
+# def profiling(df):
+#     pr = df.profile_report()
+#      # This makes the function take 2s to run
+#     return pr
+
+
+# if uploaded_f is not None:
+#     df = load_data1(uploaded_f)
+#     st.write(df.head())
+#     pr = profiling(df)
+#     st_profile_report(pr)
